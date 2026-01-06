badddb452974d1a6799b51aa85898693bcdf139e
Devarshi2285 <134229662+Devarshi2285@users.noreply.github.com>
2026-01-06 10:03:32 +0530

    Day 3 part 2 which works on larger data -DB

--- Day3/day_3_2.cpp
+++ Day3/day_3_2.cpp
@@ -2,35 +2,37 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <ctime>
+
 using namespace std;
 
 
-string extract_big_num(string number , int k , int curr_size , int n , int i , string curr_number){
-    if(curr_size>=k){
-        return curr_number;
-    }
-    if(i>=n){
-        return "";
-    }
+// string extract_big_num(string number , int k , int curr_size , int n , int i , string curr_number){
+//     if(curr_size>=k){
+//         return curr_number;
+//     }
+//     if(i>=n){
+//         return "";
+//     }
 
-    curr_number.push_back(number[i]);
-    string num1=extract_big_num(number , k ,  curr_size+1, n, i+1 , curr_number);
-    curr_number.pop_back();
-    string num2=extract_big_num(number , k ,  curr_size , n , i+1 , curr_number);
+//     curr_number.push_back(number[i]);
+//     string num1=extract_big_num(number , k ,  curr_size+1, n, i+1 , curr_number);
+//     curr_number.pop_back();
+//     string num2=extract_big_num(number , k ,  curr_size , n , i+1 , curr_number);
 
-    if(num1>=num2){
-        return num1;
-    } 
-        return num2;
+//     if(num1>=num2){
+//         return num1;
+//     } 
+//         return num2;
 
 
-}
+// }
 
 
 int main()
 {
 
-    string filename = "test.txt";
+    string filename = "input.txt";
     ifstream file(filename);
     int curr = 50;
     long long count = 0;
@@ -42,6 +44,8 @@ int main()
 
         long long ans=0;
 
+        clock_t start, end;
+        start = clock();
 
         while (getline(file, line))
         {
@@ -49,24 +53,28 @@ int main()
                 line.pop_back();
                 
             }
-            
-            
-            cout << "Read line: " << line << std::endl;
             int n=line.size();
-            string number=extract_big_num(line , 12 ,  0 , n , 0  , "");
-            cout<<number<<endl;
+            vector<vector<string>>dp(n+1,vector<string>(13,""));
+            for(int i=1 ; i<=n ; i++){
+                for(int j=1 ; j<=12 ; j++){
 
+                    string num2=dp[i-1][j-1];
 
-            //vector<vector<string>>dp(n+1,vector<string>(13,""));
+                    num2.push_back(line[i-1]);
 
+                    dp[i][j] = dp[i-1][j] > num2 ? dp[i-1][j] : num2;
+                }
 
+                // 170418192256861
+                // 170418192256861
 
-            long long num_to_add=stoll(number);
-           ans+=num_to_add;  
-
-
+            }
+            ans+=stoll(dp[n][12]);
         }
+        end = clock();
 
+        double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        std::cout << "CPU time used: " << cpu_time_used << " seconds" << std::endl;
         cout<<ans<<endl;
     }
 }
